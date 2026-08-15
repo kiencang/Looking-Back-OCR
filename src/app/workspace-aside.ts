0fc26ed9d830fa2818d4cc12479a09a18582954f
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2026-08-15 22:55:11 +0700

    Add files via upload

--- src/app/workspace-aside.ts
+++ src/app/workspace-aside.ts
@@ -363,7 +363,7 @@ export class WorkspaceAside {
       } else {
         this.showStyleSuccessBadge.set(false);
       }
-    }, { allowSignalWrites: true });
+    });
   }
 
   resetPdf = output<void>();
